e0fd35f0069dad9ed085350dc4b2d94a308744bb
ioav-livneh <ioavlivneh@gmail.com>
2025-01-18 15:28:34 +0100

    test

--- server/api/cards.js
+++ server/api/cards.js
@@ -1,46 +1,46 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+// const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = process.env.MONGO_URI;
+// const uri = process.env.MONGO_URI;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
 
-let db;
-console.log("Starting function...");
-console.log("URI:", process.env.MONGO_URI);
+// let db;
+// console.log("Starting function...");
+// console.log("URI:", process.env.MONGO_URI);
 
-// Helper to connect to the database
-async function getDb() {
-  if (!db) {
-    await client.connect();
-    db = client.db("Commonplace");
-  }
-  return db;
-}
+// // Helper to connect to the database
+// async function getDb() {
+//   if (!db) {
+//     await client.connect();
+//     db = client.db("Commonplace");
+//   }
+//   return db;
+// }
 
-module.exports = async (req, res) => {
-  try {
-    const db = await getDb();
-    const cardsCollection = db.collection("cards");
+// module.exports = async (req, res) => {
+//   try {
+//     const db = await getDb();
+//     const cardsCollection = db.collection("cards");
 
-    const agg = [
-      {
-        $match: { visible: true },
-      },
-      {
-        $sort: { topic: 1 },
-      },
-    ];
-    const cursor = cardsCollection.aggregate(agg);
-    const result = await cursor.toArray();
-    res.status(200).json(result);
-  } catch (error) {
-    console.error("Error fetching cards:", error);
-    res.status(500).send("Internal Server Error");
-  }
-};
+//     const agg = [
+//       {
+//         $match: { visible: true },
+//       },
+//       {
+//         $sort: { topic: 1 },
+//       },
+//     ];
+//     const cursor = cardsCollection.aggregate(agg);
+//     const result = await cursor.toArray();
+//     res.status(200).json(result);
+//   } catch (error) {
+//     console.error("Error fetching cards:", error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// };
--- server/server.js
+++ server/server.js
@@ -1,109 +1,109 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
-// const cors = require("cors");
-// const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// const cards = require("./cards.json");
+const cards = require("./cards.json");
 const app = express();
 const PORT = 8080;
 
-// app.use(cors());
+app.use(cors());
 
 app.use("/", (req, res) => {
   res.send("server is running.");
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
 
-// const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI;
 
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-// let db;
+let db;
 
-// async function startServer() {
-//   try {
-//     await client.connect();
-//     db = client.db("Commonplace");
-//     console.log("Successfully connected to MongoDB!");
+async function startServer() {
+  try {
+    await client.connect();
+    db = client.db("Commonplace");
+    console.log("Successfully connected to MongoDB!");
 
-//     const cardsCollection = db.collection("cards");
-//     const booksCollection = db.collection("books");
+    const cardsCollection = db.collection("cards");
+    const booksCollection = db.collection("books");
 
-//     app.get("/api/cards", async (req, res) => {
-//       try {
-//         const agg = [
-//           {
-//             $match: {
-//               visible: true,
-//             },
-//           },
-//           {
-//             $sort: {
-//               topic: 1,
-//             },
-//           },
-//         ];
-//         const cards = await cardsCollection;
-//         const cursor = cards.aggregate(agg);
-//         const result = await cursor.toArray();
-//         res.json(result);
-//       } catch (error) {
-//         // res.json(cards);
-//         console.error("Error fetching cards:", error);
-//         res.status(500).send("Internal Server Error");
-//       }
-//     });
+    app.get("/api/cards", async (req, res) => {
+      try {
+        const agg = [
+          {
+            $match: {
+              visible: true,
+            },
+          },
+          {
+            $sort: {
+              topic: 1,
+            },
+          },
+        ];
+        const cards = await cardsCollection;
+        const cursor = cards.aggregate(agg);
+        const result = await cursor.toArray();
+        res.json(result);
+      } catch (error) {
+        // res.json(cards);
+        console.error("Error fetching cards:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
 
-//     app.get("/api/books", async (req, res) => {
-//       try {
-//         const agg = [
-//           {
-//             $sort: {
-//               dateAdded: 1,
-//             },
-//           },
-//         ];
-//         const books = await booksCollection;
-//         const cursor = books.aggregate(agg);
-//         const result = await cursor.toArray();
-//         res.json(result);
-//       } catch (error) {
-//         console.error("Error fetching cards:", error);
-//         res.status(500).send("Internal Server Error");
-//       }
-//     });
+    app.get("/api/books", async (req, res) => {
+      try {
+        const agg = [
+          {
+            $sort: {
+              dateAdded: 1,
+            },
+          },
+        ];
+        const books = await booksCollection;
+        const cursor = books.aggregate(agg);
+        const result = await cursor.toArray();
+        res.json(result);
+      } catch (error) {
+        console.error("Error fetching cards:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
 
-//     app.listen(PORT, () => {
-//       console.log(`Server started on port ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//     process.exit(1); // Exit process if connection fails
-//   }
-// }
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit process if connection fails
+  }
+}
 
-// startServer();
+startServer();
 
-// // Handle graceful shutdown
-// process.on("SIGINT", async () => {
-//   console.log("Received SIGINT. Closing MongoDB connection...");
-//   await client.close();
-//   console.log("MongoDB connection closed.");
-//   process.exit(0); // Exit process
-// });
+// Handle graceful shutdown
+process.on("SIGINT", async () => {
+  console.log("Received SIGINT. Closing MongoDB connection...");
+  await client.close();
+  console.log("MongoDB connection closed.");
+  process.exit(0); // Exit process
+});
 
-// process.on("SIGTERM", async () => {
-//   console.log("Received SIGTERM. Closing MongoDB connection...");
-//   await client.close();
-//   console.log("MongoDB connection closed.");
-//   process.exit(0); // Exit process
-// });
+process.on("SIGTERM", async () => {
+  console.log("Received SIGTERM. Closing MongoDB connection...");
+  await client.close();
+  console.log("MongoDB connection closed.");
+  process.exit(0); // Exit process
+});
